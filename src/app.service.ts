92523aae6a52b13939dbc50e89699bd353d881f9
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-03 23:27:20 +0700

    feat(app): add `@InjectRepository(Event)`

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from './entities';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Event) private eventRepository: Repository<Event>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
   }
