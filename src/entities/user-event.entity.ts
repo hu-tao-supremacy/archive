410db0a68c39ebe011e7df04f54d994abd817c53
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-03 23:19:36 +0700

    chore(entities): remove GraphQL decorators

--- src/entities/user-event.entity.ts
+++ src/entities/user-event.entity.ts
@@ -1,48 +1,38 @@
-import { UserEvent_Status as Status } from '@onepass/graphql/common/common';
-import { Field, InputType, Int, IntersectionType, ObjectType, OmitType, registerEnumType } from '@nestjs/graphql';
-import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Event } from './event.entity';
 import { User } from './user.entity';
-import { pick } from 'lodash';
 
-export const PickedUserEventStatus = pick(Status, ['APPROVED', 'REJECTED', 'PENDING']);
-registerEnumType(PickedUserEventStatus, { name: 'UserEventStatus' });
-
-@InputType('UserEventInput')
-@ObjectType()
 @Index(['userId', 'eventId'], { unique: true })
 @Index(['eventId', 'ticket'], { unique: true })
 @Entity()
 export class UserEvent {
-  @Field((_) => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Field((_) => Int)
   @Column()
   userId: number;
 
-  @Field((_) => User)
   @ManyToOne(() => User, { onDelete: 'CASCADE' })
   user: User;
 
-  @Field((_) => Int)
   @Column()
   eventId: number;
 
-  @Field((_) => Event)
   @ManyToOne(() => Event, { onDelete: 'CASCADE' })
   event: Event;
 
-  @Field((_) => Int, { nullable: true })
   @Column({ nullable: true })
   rating?: number;
 
-  @Field({ nullable: true })
   @Column({ nullable: true })
   ticket?: string;
 
-  @Field((_) => PickedUserEventStatus)
   @Column('enum', { enum: ['PENDING', 'APPROVED', 'REJECTED'] })
   status: string;
 
--- src/entities/user-permission.entity.ts
+++ src/entities/user-permission.entity.ts
@@ -1,26 +1,24 @@
-import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
-import { Permission } from '@onepass/graphql/common/common';
-import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { UserOrganization } from './user-organization.entity';
 
-@InputType('UserPermissionInput')
-@ObjectType()
 @Index(['userOrganizationId', 'permissionName'], { unique: true })
 @Entity()
 export class UserPermission {
-  @Field((_) => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Field((_) => Int)
   @Column()
   userOrganizationId: number;
 
-  @Field((_) => UserOrganization)
   @ManyToOne(() => UserOrganization, { onDelete: 'CASCADE' })
   userOrganization: UserOrganization;
 
-  @Field()
   @Column('text')
-  permissionName: Permission;
+  permissionName: string;
 }
