76800f0af02f36506add5bb3806dab191faed71e
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-04 01:45:06 +0700

    feat(archive): add createExportable()

--- src/archive/archive.service.ts
+++ src/archive/archive.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Event, UserEvent } from 'src/entities';
+import * as flat from 'flat';
+import { Event, UserEvent, Question, QuestionGroup } from 'src/entities';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -23,6 +24,57 @@ export class ArchiveService {
       ],
     });
 
-    console.log(JSON.stringify(userEvents, null, 2));
+    let exports = userEvents.map((userEvent) =>
+      this.createExportable(userEvent),
+    );
+
+    console.log(JSON.stringify(exports, null, 2));
+  }
+
+  createExportable(userEvent: UserEvent) {
+    const user = userEvent.user;
+    const answers = userEvent.answers;
+
+    let questionGroups = userEvent.event.questionGroups;
+
+    questionGroups.forEach((questionGroup) => {
+      questionGroup.questions.sort((a: Question, b: Question) => a.seq - b.seq);
+    });
+
+    questionGroups = questionGroups.sort(
+      (a: QuestionGroup, b: QuestionGroup) => a.seq - b.seq,
+    );
+
+    const exportable: Record<any, any> = {};
+    exportable.questions = {};
+
+    questionGroups.forEach((questionGroup) => {
+      questionGroup.questions.forEach((question) => {
+        exportable.questions[
+          `${questionGroup.seq}.${question.seq} - ${questionGroup.title} - ${question.title}`
+        ] = answers.find((answer) => answer.questionId === question.id).value;
+      });
+    });
+
+    exportable.id = userEvent.id;
+    exportable.ticket = userEvent.ticket ?? '';
+    exportable.status = userEvent.status;
+
+    exportable.user = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      nickname: user.nickname ?? '',
+      chulaId: user.chulaId ?? '',
+      gender: user.gender,
+      academicYear: user.academicYear ?? '',
+      phoneNumber: user.phoneNumber ?? '',
+      email: user.email,
+      district: user.district ?? '',
+      province: user.province ?? '',
+      zipCode: user.zipCode ?? '',
+      address: user.address ?? '',
+    };
+
+    return flat(exportable);
   }
 }
