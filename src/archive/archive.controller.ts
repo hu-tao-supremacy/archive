0175dd50f30ddac74912f9d7e73b222ab753b7e7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-04 17:08:41 +0700

    feat: update

--- src/archive/archive.controller.ts
+++ src/archive/archive.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Header, Param, Post, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Header,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { ArchiveService } from './archive.service';
 import { Response } from 'express';
 import { stringify } from 'csv';
@@ -9,16 +17,19 @@ export class ArchiveController {
   constructor(private readonly archiveService: ArchiveService) {}
 
   @Get('/:archiveKey')
-  @Header('Content-Type', 'text/csv')
   async generateCSV(
     @Param('archiveKey') archiveKey: string,
     @Res() res: Response,
   ) {
     const eventId = this.archiveService.getEventId(archiveKey);
+    if (!eventId) {
+      throw new BadRequestException('Archive Key is invalid or expired.');
+    }
     const exportableObjects = await this.archiveService.makeExportableObjects(
       eventId,
     );
 
+    res.setHeader('Content-Type', 'text-csv');
     res.setHeader(
       'Content-Disposition',
       'attachment; filename=' + nanoid() + '.csv',
