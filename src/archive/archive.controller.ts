c8a9365c09bc393f4bdc4eb650a4658597ec1f5a
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-04 16:41:53 +0700

    feat: set Content-Disposition header

--- src/archive/archive.controller.ts
+++ src/archive/archive.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Header, Param, Post, Res } from '@nestjs/common';
 import { ArchiveService } from './archive.service';
 import { Response } from 'express';
 import { stringify } from 'csv';
+import { nanoid } from 'nanoid';
 
 @Controller('archive')
 export class ArchiveController {
@@ -12,6 +13,11 @@ export class ArchiveController {
   async generateCSV(@Param('eventId') eventId: number, @Res() res: Response) {
     const exportableObjects = await this.archiveService.makeExportableObjects(
       eventId,
+    );
+
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename=' + nanoid() + '.csv',
     );
     stringify(exportableObjects, { header: true }).pipe(res);
   }
