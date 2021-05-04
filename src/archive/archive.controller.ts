76ea3008570a8be7de1da1a8b48ed7a0ede127e8
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-04 16:36:09 +0700

    feat: add Content-Type = text/csv to generateCSV()

--- src/archive/archive.controller.ts
+++ src/archive/archive.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Controller, Get, Header, Param, Post, Res } from '@nestjs/common';
 import { ArchiveService } from './archive.service';
 import { Response } from 'express';
 import { stringify } from 'csv';
@@ -8,6 +8,7 @@ export class ArchiveController {
   constructor(private readonly archiveService: ArchiveService) {}
 
   @Get('/:eventId')
+  @Header('Content-Type', 'text/csv')
   async generateCSV(@Param('eventId') eventId: number, @Res() res: Response) {
     const exportableObjects = await this.archiveService.makeExportableObjects(
       eventId,
